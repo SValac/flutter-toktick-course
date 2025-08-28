31e2847c577243613f274fd4037bab899eb9c093
Jose Isaac Blanco Sandoval <4673800+SValac@users.noreply.github.com>
2025-08-28 01:07:33 -0600

    app toktik sin videos

---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/full_screen_player.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VideoScrollableView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      physics: BouncingScrollPhysics(),
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            //video player + gradiente
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                videoUrl: videoPost.videoUrl,
+                caption: videoPost.caption,
+              ),
+            ),
+            //botones
+            Positioned(
+              right: 20,
+              bottom: 40,
+              child: VideoButtons(video: videoPost),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
